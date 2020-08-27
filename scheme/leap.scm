660200089634bf63e1ca1a37f737e1dcabdf35b1
Virgil <contact@p-ku.com>
2020-08-27 07:22:07 -0500

    Use zero? instead of = to check divisibility

--- scheme/leap.scm
+++ scheme/leap.scm
@@ -1,6 +1,6 @@
 (import (rnrs))
 
 (define (leap-year? year)
-  (define (divis? x) (= (remainder year x) 0))
+  (define (divis? x) (zero? (remainder year x)))
   (or (and (divis? 4) (divis? 100) (divis? 400))
       (and (divis? 4) (not (divis? 100)))))
