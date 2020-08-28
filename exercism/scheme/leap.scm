b19c3c287d1fdc3ec53bf5d464cb931c4e09db52
Virgil <contact@p-ku.com>
2020-08-27 20:30:45 -0500

    Remove unnecessary and statement

--- exercism/scheme/leap.scm
+++ exercism/scheme/leap.scm
@@ -2,5 +2,5 @@
 
 (define (leap-year? year)
   (define (divis? x) (zero? (remainder year x)))
-  (or (and (divis? 4) (divis? 100) (divis? 400))
+  (or (divis? 400)
       (and (divis? 4) (not (divis? 100)))))
